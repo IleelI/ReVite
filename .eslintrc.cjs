09c3d70ab2dfb6ef2f3d20c207037e5ca9f6bc10
IleelI <elert.bartosz@gmail.com>
2022-09-11 23:20:03 +0200

    added eslint simple import plugin

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,8 +19,10 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
+  plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+import './assets/styles/main.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './assets/styles/main.css'
+
 import Root from './Root'
 
 const rootContainer = document.getElementById('root')
